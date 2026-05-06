7f5b63fe2e042d392c098d8df022eb82a5d1943a
Kevin Vu te Laar <vu.te@rwth-aachen.de>
2026-05-06 16:14:16 +0200

    add missing grpc dependency for python runtime
    Packages in `deps.sh` are dependencies for compilation.
    Adding grpcio, grpcio-tools was wrong here, unless necessary for CI.
    
    Signed-off-by: Kevin Vu te Laar <vu.te@rwth-aachen.de>

--- packaging/deps.sh
+++ packaging/deps.sh
@@ -148,9 +148,7 @@ python3 -m pip install \
     --prefix=${PIP_PREFIX} \
     cmake==3.31.6 \
     meson==1.9.1 \
-    ninja==1.11.1.4 \
-    grpcio \
-    grpcio-tools
+    ninja==1.11.1.4
 
 # Build & Install Criterion
 if ! pkg-config "criterion >= 2.4.1" && \
